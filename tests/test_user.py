d5b97db1365153fa31382bf8392892805d8cb7dc
HERITAGE-XION <iheritage934@gmail.com>
2025-12-08 00:24:18 +0100

    added testing for the user endpoint

---
+++ tests/test_user.py
@@ -0,0 +1,6 @@
+from app.schemas.user import Get_current_user
+
+
+def test_loggedin_user(get_loggedIn_user, test_user):
+    assert get_loggedIn_user[0].status_code == 200
+    assert get_loggedIn_user[1].email == test_user["username"]
